aee0efa9075a6fd1e65473bcb5d1d9b95577c22a
Joni Korpela <joni.korpela@student.oulu.fi>
2024-03-13 13:53:39 +0200

    commit 1

--- src/main/java/com/guiexample/GUIExample.java
+++ src/main/java/com/guiexample/GUIExample.java
@@ -3,8 +3,9 @@ package com.guiexample;
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class GUIExample {
+public class GUIExample{
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -13,16 +14,15 @@ public class GUIExample {
 
     public GUIExample(){
 
-        JButton button = new JButton("Test Button");
-
+        
         UIPanel panel = new UIPanel();
         UIFrame frame = new UIFrame();
-
+        CounterLabel label = new CounterLabel(30, 30, 30, 30, "Click count:");
+        OpButton button = new OpButton(30, 30, 30, 30, "Click me", label);
+        panel.add(button);
+        panel.add(label);
     
         frame.add(panel, BorderLayout.CENTER);
-
-        button.setBounds(50,100,95,30);  
-        panel.add(button);
     }
     
 }
